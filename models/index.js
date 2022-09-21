67fe340613752272813527542bb0c24ab6947fbf
Pablo Charamello <pablocharamello@gmail.com>
2022-09-21 13:03:15 -0300

    backend ok, befor redeploy

--- models/index.js
+++ models/index.js
@@ -5,6 +5,7 @@ const sequelize = new Sequelize(
   process.env.DB_USERNAME,
   process.env.DB_PASSWORD,
   {
+    port: process.env.DB_PORT,
     host: process.env.DB_HOST,
     dialect: process.env.DB_CONNECTION,
     dialectModule: require("pg"),
