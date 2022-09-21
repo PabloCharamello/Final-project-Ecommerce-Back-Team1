995bc6bb5d1bcd769cdc92ab3a34af6500584e51
Pablo Charamello <pablocharamello@gmail.com>
2022-09-21 13:10:41 -0300

    server.js commit line 6

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const routes = require("./routes");
-const dbInitialSetup = require("./dbInitialSetup");
+// const dbInitialSetup = require("./dbInitialSetup");
 const APP_PORT = process.env.APP_PORT || 8000;
 const app = express();
 
